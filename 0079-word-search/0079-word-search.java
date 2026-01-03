3a38727ae2d699c7d274facf17a7d06a22f74020
kartik patel <kartikpatell4july1997@gmail.com>
2026-01-03 11:42:56 +0530

    Time: 129 ms (82.95%), Space: 43.2 MB (29.49%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -5,14 +5,14 @@ class Solution {
             for (int j = 0; j < board[0].length; j++) {
                 // Step 2: Start DFS if the first character matches
                 if (board[i][j] == word.charAt(0)) {
-                    if (search(i, j, 0, board, word)) return true;
+                    if (dfs(i, j, 0, board, word)) return true;
                 }
             }
         }
         return false;
     }
 
-    private boolean search(int i, int j, int index, char[][] board, String word) {
+    private boolean dfs(int i, int j, int index, char[][] board, String word) {
         // Base Case: All characters in 'word' have been found
         if (index == word.length()) return true;
 
@@ -27,10 +27,10 @@ class Solution {
         board[i][j] = '#'; // Use a special character to mark as visited
 
         // Step 4: Explore all 4 directions (Up, Down, Left, Right)
-        boolean found = search(i + 1, j, index + 1, board, word) ||
-                        search(i - 1, j, index + 1, board, word) ||
-                        search(i, j + 1, index + 1, board, word) ||
-                        search(i, j - 1, index + 1, board, word);
+        boolean found = dfs(i + 1, j, index + 1, board, word) ||
+                        dfs(i - 1, j, index + 1, board, word) ||
+                        dfs(i, j + 1, index + 1, board, word) ||
+                        dfs(i, j - 1, index + 1, board, word);
 
         // Step 5: Backtrack - restore the cell's original value
         board[i][j] = temp;
