df1323498561bf118e6d2bfef99e5dc78108b0ee
kartik patel <kartikpatell4july1997@gmail.com>
2026-01-18 09:58:31 +0530

    Time: 4 ms (98.22%), Space: 48 MB (24.95%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -2,20 +2,20 @@ class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         int n = arr.length;
         List<Integer> res = new ArrayList<>();
-        int startPosMin = 0; int startPosMax=n-k;
-        while(startPosMin<startPosMax){ //0(n-k)
-             int leftBoundary = arr[startPosMin];
-             int rightBoundary = arr[startPosMax+k-1];
-             if( Math.abs(leftBoundary-x)> Math.abs(rightBoundary-x))
-                startPosMin++;
-             else
-                 startPosMax--;  
-        } 
+        int left = 0; int right=n-k;
+        //binary search on possible valid starting windows positions [ 0,n-k]
+        while(left<right){
+            int mid = left+(right-left)/2;
+            if(x-arr[mid] > arr[mid+k]-x)
+                left=mid+1;
+            else
+                right=mid;   
+        }
 
-        for(int i=startPosMin; i<startPosMin+k; i++){  //0(k)
+        for(int i = left; i<left+k;i++){
             res.add(arr[i]);
         }
         return res;
-        //total complexity = 0(1)+0(n-k)+0(k) = 0(n-k+k) = 0(n)
+        //total complexity = 0(1)+0(log(n-k))+0(k) = 0(log(n-k)+0(k))
     }
 }
