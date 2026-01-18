a599f2a2e25b3418bec5616744b2a97621a80161
kartik patel <kartikpatell4july1997@gmail.com>
2026-01-18 11:42:53 +0530

    Time: 66 ms (64.91%), Space: 86.9 MB (7.51%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,36 +1,45 @@
-
-class pair{
-    int index;
-    int height;
-
-    public pair(int index,int height){
-        this.index=index;
-         this.height=height;
-    }
-}
 class Solution {
+    class pair {
+        int index;
+        int height;
+
+       public pair(int index,int height){
+            this.index=index;
+            this.height=height;
+        }
+    }
+
     public int largestRectangleArea(int[] heights) {
         Stack<pair> s = new Stack<>();
-        int maxarea=0;
-         int n = heights.length;
-        for(int i =0; i<heights.length;i++){
-            int start = i;
+        int start = 0;
+        int n = heights.length;
+        int maxarea = -1;
 
-            while(!s.isEmpty() && s.peek().height>heights[i]){
-                int index = s.peek().index;
+        for (int i = 0; i < n; i++) {
+            //assume current bar can be extended to left till current bar index i hence start = i
+            start = i;
+            //while popping greater height bar keep updating start = popped index 
+            //meaning we are extending current bar width as big as possible
+            while (!s.isEmpty() && s.peek().height > heights[i]) {
                 int height = s.peek().height;
-                maxarea =Math.max(maxarea, height *(i-index));
-                start = index;
+                int index = s.peek().index;
+                int width = i - index;
+                //calculate area for each stack top bar popped coz it cant be extended to right more and its             left index is already present in current stack pair
+                maxarea = Math.max(maxarea, height * width); 
                 s.pop();
+                start=index; // extending current bar width to left as much as possible
             }
-            s.push(new pair(start,heights[i]));
+            s.push(new pair(start, heights[i]));
         }
-
-        while(!s.isEmpty()){
-             int index = s.peek().index;
-             int height = s.peek().height;
-             maxarea = Math.max(maxarea, height*(n-index));
-             s.pop();
+         
+         //remaining height in stack can be extended till last n = height.length
+         // and its index means it can be extended till that index hence width = n-index
+        while (!s.isEmpty()) {
+            int height = s.peek().height;
+            int index = s.peek().index;
+            int width = n-index;
+            maxarea = Math.max(maxarea,height*width);
+            s.pop();
         }
         return maxarea;
     }
