64961198690b979b6a7af8a64b584a1ad3e61584
kartik patel <kartikpatell4july1997@gmail.com>
2026-02-04 12:03:41 +0530

    Time: 1 ms (99.45%), Space: 48.7 MB (92.1%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -7,14 +7,13 @@ class Solution {
         int[] b = m <= n ? nums2 : nums1;
 
         int total = m + n;
+        int low = 0;
+        int high = a.length;
         int half = total / 2;
-
-        int low = 0, high = a.length;
 
         while (true) {
             int i = (low + high) / 2;
             int j = half - i;
-
             int aleft = i > 0 ? a[i - 1] : Integer.MIN_VALUE;
             int aright = i < a.length ? a[i] : Integer.MAX_VALUE;
 
@@ -24,14 +23,13 @@ class Solution {
             if (aleft <= bright && bleft <= aright) {
                 if (total % 2 == 0) {
                     return (Math.max(aleft, bleft) + Math.min(aright, bright)) / 2.0;
-                } else {
+                } else
                     return Math.min(aright, bright);
-                }
-            } else if (aleft > bright) {
+            } else if (aleft > bright)
                 high = i - 1;
-            } else {
+            else
                 low = i + 1;
-            }
+
         }
     }
 }
